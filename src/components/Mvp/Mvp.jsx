0c6523ebeb61483ac0cb8259f6c0e929b393f6ad
David Rojas <deznorth@gmail.com>
2019-03-17 13:09:25 -0400

    Added the missing h2 to the MVP

--- src/components/Mvp/Mvp.jsx
+++ src/components/Mvp/Mvp.jsx
@@ -8,6 +8,7 @@ const Mvp = () => {
         <div className="mvp">
           <img src={logo} alt="Hellohaidy's Logo" width="300px"/>
           <h1>Hellohaidy</h1>
+          <h2>Advertising // Photography // Videography</h2>
           <p>Haidy D. Andrada is an advertiser, with a variety of passions. She is currently a student at the University of North Florida studying for a dual degree in communications with a focus in advertising and in interdisciplinary studies with a theme based on music entrepreneurship. Recently she coordinated and advertised an event “The Latin Night at the Boathouse” She is seeking an internship in advertising and strives to learn more about the entertainment industry first hand. Haidy is honing her craft in photography and videography for content creation on social media advertising.</p>
           <SocialBar />
         </div>
